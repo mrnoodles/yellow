c10a0f9a620d00d0d952afed2dc8518b6baec509
Andres Gonzalez <andresgl@live.com>
2015-06-05 19:32:07 -0400

    Added Structure

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 __author__ = 'user'
+
+import pygame
+import keyboard
+import physics
+import mixer
+import graphics
+import pokedex
+
+def main():
+
+    controller()
+    model()
+    view()
+
+
+def controller():
+    pass
+
+def model():
+    pass
+
+def view():
+    pass
+
+if __name__ == "__main__":
+    pygame.init()
+    main()
