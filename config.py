c6b84e66b99da0177f7b2494beab654c14c6f9ba
Carlos N. Abreu Takemura <Naoto@Naotos-MBA.local>
2015-06-12 21:10:54 -0700

    Added .gitignore and a debug flag in config.py

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 __author__ = 'Naoto'
-
+"""
+The purpose of this file is to hold game-wide configurations
+that are not part of the actual game's logic.
+"""
 
 #  Window Configurations
 WIDTH = 1366
@@ -8,3 +11,6 @@ WINDOW_DIMENSIONS = (WIDTH, HEIGHT)
 
 #  Game's FPS
 FPS = 60
+
+#  Debug Mode
+DEBUG = False
