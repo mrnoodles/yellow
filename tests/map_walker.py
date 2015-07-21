ad52f2008e47d264c0af0d3a756522569ba9fc52
andres <andres.gonzalez2@upr.edu>
2015-07-20 21:05:55 -0400

    Refactored some image names.
    Added base of walking code.

--- tests/map_walker.py
+++ tests/map_walker.py
@@ -23,9 +23,9 @@ import config
 import graphics
 from models import navigation_registry
 from models import context
-
+from controllers import one_and_only as joypad
 from maps import locations
-from maps import maps
+from maps import legends
 import mixer
 
 def main():
@@ -40,13 +40,14 @@ def setup():
     where = locations.MY_HOUSE
     state.navigation_registry = navigation_registry.Navigator(where, where.map_data[1], (3, 3))
     state.window = pygame.display.set_mode(config.WINDOW_DIMENSIONS)
+    state.change_controller("standing")
 
 
     return state
 
 def game_loop(state):
     sys_clock = pygame.time.Clock()
-    frame_count = 0
+    state.global_frame_count = 0
 
     while True:
         handle_events(state)
@@ -55,16 +56,39 @@ def game_loop(state):
 
         pygame.display.update()
         sys_clock.tick(config.FPS)
-        frame_count += 1
+        state.global_frame_count += 1
 
 def handle_events(state):
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
+        state.direction = None
+        if state.controller == "standing":
+            state.direction = joypad.movement(pygame.key.get_pressed())
+
 def game_logic(state):
-    pass
+    if state.controller == "standing":
+        direction = state.direction
+        pos = state.navigation_registry.position
+        state.destination = pos[0] + direction[0], pos[1] + direction[1]
+
+        map = state.navigation_registry.map
+
+        if map.contains_position(state.destination):
+            dest_type = map.walkable[state.destination[0]][state.destination[1]]
+            value = legends.walkable_legend[dest_type%100]
+            argument = dest_type/100
+
+            if (value == "WALKABLE") or (value == "WARP"):
+                state.change_controller("walking")
+            else:
+                state.change_controller("thud")
+        else:
+            state.change_controller("thud")
+
 
 def draw(state):
     area = state.navigation_registry.map
